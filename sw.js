f1864ae69cb28e3aa8903478c1eb8c881788f87c
Ludovic JORGE DO MARCO <ljorgedomarco@MacBook-Air-de-Ludovic.local>
2026-05-25 14:40:47 +0200

    chore(pwa):add icons

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "budget-v1";
-const SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
+const CACHE = "budget-v2";
+const SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(SHELL)).then(() => self.skipWaiting()));
